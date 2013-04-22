1ed2e95df002e28b99dbccab0a76aa9d9becbb3f
Carlos Forti <carlaoforti@gmail.com>
2013-04-22 15:06:04 -0300

    Trazendo do GitHub

---
+++ Ffsti.Library.ConsultaCep/ConsultaCep.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Ffsti.Library.ConsultaCep
+{
+    public struct Cep
+    {
+        public string CepPesquisado;
+        public int Resultado;
+        public string ResultadoTxt;
+        public string Uf;
+        public string Cidade;
+        public string Bairro;
+        public string TipoLogradouro;
+        public string Logradouro;
+
+        public override string ToString()
+        {
+            return CepPesquisado + " / " + Resultado + " / " + ResultadoTxt + " / " + Uf + " / " + Cidade + " / " + Bairro + " / " + TipoLogradouro + " / " + Logradouro;
+        }
+    }
+
+    public static class ConsultaCep
+    {
+        private static string WEBSERVICE_URL_BASE = "http://webservice.kinghost.net/web_cep.php?auth={0}&formato=xml&cep={1}";
+
+        public static Cep GetCep(string authKey, string cep)
+        {
+            string url = string.Format(WEBSERVICE_URL_BASE, authKey, cep);
+            //XmlTextReader reader = new XmlTextReader(url);
+            XDocument doc = XDocument.Load(url);
+
+            Cep cepRetorno = new Cep();
+
+            cepRetorno.CepPesquisado = cep;
+
+            cepRetorno.Resultado = (from d in doc.Descendants("webservicecep")
+                                    select (int)d.Element("resultado")).FirstOrDefault();
+
+            cepRetorno.ResultadoTxt = (from d in doc.Descendants("webservicecep")
+                                       select (string)d.Element("resultado_txt")).FirstOrDefault();
+
+            cepRetorno.Uf = (from d in doc.Descendants("webservicecep")
+                             select (string)d.Element("uf")).FirstOrDefault();
+
+            cepRetorno.Cidade = (from d in doc.Descendants("webservicecep")
+                                 select (string)d.Element("cidade")).FirstOrDefault();
+
+            cepRetorno.Bairro = (from d in doc.Descendants("webservicecep")
+                                 select (string)d.Element("bairro")).FirstOrDefault();
+
+            cepRetorno.TipoLogradouro = (from d in doc.Descendants("webservicecep")
+                                         select (string)d.Element("tipo_logradouro")).FirstOrDefault();
+
+            cepRetorno.Logradouro = (from d in doc.Descendants("webservicecep")
+                                     select (string)d.Element("logradouro")).FirstOrDefault();
+
+            return cepRetorno;
+        }
+    }
+}
---
+++ Ffsti.Library.Testes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ffsti.Library.Testes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ConsultaCep.ConsultaCep.GetCep("881296b989d80fdb142000196467080d", "13416030"));
+            Console.WriteLine();
+            Console.WriteLine(ConsultaCep.ConsultaCep.GetCep("881296b989d80fdb142000196467080d", "13390000"));
+            Console.WriteLine();
+            Console.WriteLine(ConsultaCep.ConsultaCep.GetCep("881296b989d80fdb142000196467080d", "13416016"));
+            Console.ReadKey();
+        }
+    }
+}
