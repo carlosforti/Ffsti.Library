710692c61f3067cdac1ec7096d4cd9bcf082c3a1
Carlos Forti <carlos.forti@fortieforti.com.br>
2013-05-22 08:51:42 -0300

    Summary in Enums

--- Ffsti.Library/IntegerExtensionMethods.cs
+++ Ffsti.Library/IntegerExtensionMethods.cs
@@ -5,8 +5,15 @@ using System.Text;
 
 namespace Ffsti
 {
+	/// <summary>
+	/// Classe de métodos de extensão para inteiros
+	/// </summary>
 	public static class IntegerExtensionMethods
 	{
+		/// <summary>
+		/// Calcula o fatorial de um dado numero
+		/// </summary>
+		/// <returns></returns>
 		public static long Fatorial(this int value)
 		{
 			if (value == 1)
